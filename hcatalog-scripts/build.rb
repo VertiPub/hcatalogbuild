0e0c0b1f0144109e6667687d3748f3a8d230ef9f
Lee T. Tucker <ltucker@verticloud.com>
2013-02-08 08:19:33 -0800

    Enter more assumptions, construct the build command

--- hcatalog-scripts/build.rb
+++ hcatalog-scripts/build.rb
@@ -10,6 +10,14 @@ Assumptions:
 	1.  WORKSPACE is defined, either via execution within Jenkins or by the user
 	2.  The "wrapper" repository has been cloned and the version checked out for use.
         3.  The "wrapper" repository exists at the level of WORKSPACE (ala Jenkins)
+        4.  The build script will generate the necessary artifacts to be used by the install script
+            a.  The build script will be passed an "ARTIFACT_VERSION" environment variable
+            b.  The build script will be passed the "DATESTRING" environment variable should it need it
+            c.  The build script will inherit the "WORKSPACE"  environment variable
+            d.  The build script will execute in the submodule directory
+            e.  The build script will have a non-zero exit if the build fails for any reason
+        5.  The install script will generate the proper build root when passed the correct install directory
+
 
 =end
 
@@ -21,6 +29,7 @@ class ParseOptions < Hash
     self[:externalversion] = ''
     self[:branchortag] = ''
     self[:subrepo] = ''
+    self[:compilescript] = ''
 
     # define the opts and usage
     opts = OptionParser.new do |opts|
@@ -35,6 +44,10 @@ class ParseOptions < Hash
       opts.on('-s', '--submodule DIRECTORY',
               'The directory containing the sub-repository relative to the build root') do |string|
         self[:submodule] = string
+      end
+      opts.on('-c', '--compilescript SCRIPTNAME',
+              'The bash script that runs the actual compile commands from the submodule directory') do |string|
+        self[:compilescript] = string
       end
       opts.on_tail('-h', '--help', 'display this help and exit') do
         puts opts
@@ -64,6 +77,10 @@ class ParseOptions < Hash
       puts opts
       raise "FATAL: You must specify the submodule directory to build, relative to the build root"
     end
+    if (self[:compilescript] == '')
+      puts opts
+      raise "FATAL: You must specify the path to the compile script relative to the build root"
+    end
   end
 end
 
@@ -80,20 +97,27 @@ buildString = buildTime.strftime("%Y%m%d%H%M")
 # Save the starting directory to return to
 startingDir = Dir.pwd
 
-# Move to the workspace defined by hudson and initialize/update the repos
+# Move to the workspace defined by hudson and initialize/update the submodule repo
 Dir.chdir(workSpace)
 system "git submodule init"
 system "git submodule update"
 
 # Check out the correct branch of code in preparation to call the build
+# BUG: gitignore the necessary directories that way we won't have to hassle with them
 buildDir = workSpace + "/" + arguments[:submodule]
 Dir.chdir(buildDir)
 branchName = "tobebuilt-" + buildString
 command = "git checkout -b " + branchName + " " + arguments[:branchortag]
+system command
 
 # Return to the workspace and prepare 2 directories, one for the RPM(s) being generated
 # and the other for the file system used to emulate the installed root
 installDir = workSpace + "/install-" + buildString
 rpmDir = workSpace + "/rpms-" + buildString
 Dir.mkdir (rpmDir, "0755") 
+
+# Ok, time to call out to the actual build command
+scriptFullPath = workspace + "/" + arguments[:compilescript]
+buildCommand = "/bin/sh -ex " + scriptFullPath
+system buildCommand
 
