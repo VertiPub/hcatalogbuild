59d1c0d1d2a483a23c6bd92152943dc9dba4229c
Lee T. Tucker <ltucker@verticloud.com>
2013-02-08 08:04:43 -0800

    add the basic build structure within the workspace

--- hcatalog-scripts/build.rb
+++ hcatalog-scripts/build.rb
@@ -5,6 +5,12 @@ It assumes a wrapper git repository with a submodule of the actual project to be
 It's a first attempt make this more modular so it can be refactored to also be used to 
 build other projects via a shared common script.
 
+Assumptions:
+
+	1.  WORKSPACE is defined, either via execution within Jenkins or by the user
+	2.  The "wrapper" repository has been cloned and the version checked out for use.
+        3.  The "wrapper" repository exists at the level of WORKSPACE (ala Jenkins)
+
 =end
 
 require 'optparse'
@@ -63,19 +69,31 @@ end
 
 # Begin with arguments
 arguments = ParseOptions.new(ARGV)
+# BUG: add a test to confirm definition of WORKSPACE
+workSpace = ENV['WORKSPACE]'
 
 # Build the Date String for the "build string"
 
 buildTime = Time.now
-
 buildString = buildTime.strftime("%Y%m%d%H%M")
 
-workSpace = ENV['WORKSPACE]'
+# Save the starting directory to return to
+startingDir = Dir.pwd
+
+# Move to the workspace defined by hudson and initialize/update the repos
 Dir.chdir(workSpace)
 system "git submodule init"
 system "git submodule update"
-buildDir = workspace + "/" + arguments[:submodule]
-Dir.chdir(buildDir)
-#git checkout -b tobebuilt release-0.4.0
 
+# Check out the correct branch of code in preparation to call the build
+buildDir = workSpace + "/" + arguments[:submodule]
+Dir.chdir(buildDir)
+branchName = "tobebuilt-" + buildString
+command = "git checkout -b " + branchName + " " + arguments[:branchortag]
+
+# Return to the workspace and prepare 2 directories, one for the RPM(s) being generated
+# and the other for the file system used to emulate the installed root
+installDir = workSpace + "/install-" + buildString
+rpmDir = workSpace + "/rpms-" + buildString
+Dir.mkdir (rpmDir, "0755") 
 
