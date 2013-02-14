f8457d15ab23bdff6431c204f8a073c0a34f8fa6
Lee T. Tucker <ltucker@verticloud.com>
2013-02-14 10:33:32 -0800

    reset the scripts becuase hacatalog has changed

--- hcatalog-scripts/build-0.4.0.sh
+++ hcatalog-scripts/build-0.4.0.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
 # you have to make this directory because git doesn't create it automatically
-rm -rf $WORKSPACE/hcatalog/lib
-mkdir $WORKSPACE/hcatalog/lib
+rm -rf ${WORKSPACE}/hcatalog/lib
+mkdir ${WORKSPACE}/hcatalog/lib
 ant -Dhcatalog.version=${ARTIFACT_VERSION} -Dforrest.home=$FORREST_HOME tar
--- hcatalog-scripts/justbuild.sh
+++ hcatalog-scripts/justbuild.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -ex
 
 # you have to make this directory because git doesn't create it automatically
+pushd ${WORKSPACE}/hcatalog
+git reset --hard
+popd
 rm -rf $WORKSPACE/hcatalog/lib
 mkdir $WORKSPACE/hcatalog/lib
 mvn versions:set -DnewVersion=${ARTIFACT_VERSION}
